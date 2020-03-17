ae22b59c5c238b99a746a71623d74dfd0f640838
Ty Udy <tyleru@qualtrics.com>
2020-03-17 01:20:15 -0600

    got it workng i think

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,7 @@
 package main
 
-func main() {
-	// resp, err := http.Get("http://gobyexample.com")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer resp.Body.Close()
+import "github.com/tylerku/go_module_example/hello"
 
-	// scanner := bufio.NewScanner(resp.Body)
-	// for i := 0; scanner.Scan() && i < 5; i++ {
-	// 	fmt.Println(scanner.Text())
-	// }
-	// if err := scanner.Err(); err != nil {
-	// 	panic(err)
-	// }
+func main() {
+	hello.Hello()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/tylerku/go_module_example
 
 go 1.13
+
+require (
+	rsc.io/quote v1.5.2
+	rsc.io/quote/v3 v3.1.0
+)
