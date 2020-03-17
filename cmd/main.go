00f1928168f857b90eb1be68c493ac7d7749302a
Ty Udy <tyleru@qualtrics.com>
2020-03-17 01:04:04 -0600

    simple go module example setup

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// resp, err := http.Get("http://gobyexample.com")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer resp.Body.Close()
+
+	// scanner := bufio.NewScanner(resp.Body)
+	// for i := 0; scanner.Scan() && i < 5; i++ {
+	// 	fmt.Println(scanner.Text())
+	// }
+	// if err := scanner.Err(); err != nil {
+	// 	panic(err)
+	// }
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tylerku/go_module_example
+
+go 1.13
